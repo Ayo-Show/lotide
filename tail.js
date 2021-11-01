b8f65a33af35326dab9c37155be68ebd68bf0b0a
Ayo-Show <sowunmi30@gmail.com>
2021-11-01 14:49:18 -0400

    Refactor tail.js

--- tail.js
+++ tail.js
@@ -1,17 +1,8 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const tail = function (array) {
+  return array.slice(1)};
 
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-const tail = array => array.slice(1);
+module.exports = tail;
 
-
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
