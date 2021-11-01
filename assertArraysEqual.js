574df674215adf9b6943656a12f865f9b812a22a
Ayo-Show <sowunmi30@gmail.com>
2021-11-01 15:26:20 -0400

    Update lotide

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-  
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
     console.log(`✅✅✅ Assertion Passed: ${array1} is equal to ${array2}`);
@@ -21,7 +7,5 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 
-
-
-
+module.exports = assertArraysEqual;
 
